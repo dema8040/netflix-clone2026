37ec13bb74987ff020b9e6864152f04b09ecdda1
dema8040 <demaye8040@gmail.com>
2026-06-11 21:42:42 +0300

    Initial Netflix Clone setup

---
+++ netflix-clone2026/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Netflix Clone</h1>
+      <p>Welcome to my Netflix Clone Project</p>
+    </div>
+  );
+}
+
+export default App;
